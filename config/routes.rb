75d9aa8bf988804b0fb800ab2f576da2a96a6478
Kikyo Kaihatsu <2311140039w@ed.fuk.kindai.ac.jp>
2025-12-03 07:12:59 +0000

    connect

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   
   # 'study_records/11/26' のようなURLパターンに対応
   get 'study_records/:month/:day', to: 'study_records#show_day', as: 'study_day'
+  
+  get 'carender', to: 'study_records#carender', as: 'carender'
+
 end
